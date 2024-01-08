c48a02e200632fa21a2b6ba9ad2df45f407fc3e6
wenke <matthieu.du@outlook.com>
2024-01-08 14:47:16 +0100

    improve: comments

--- bin/scalpel_modifier.ml
+++ bin/scalpel_modifier.ml
@@ -1,3 +1,18 @@
-type mutability = Mutable | Frozen | Static
+(** Mutablity modifier for variable, attribute and method *)
+type mutability =
+  | Mutable
+      (** A variable than can be updated at anytime. A method will update the
+          state of the object the *)
+  | Frozen
+      (** A variable that can't be updated since runtime. A method will not
+          update the state of the object. *)
+  | Static
+      (** A variable that can't be updated since compile time. A method will not
+          update the state of the object since the compile time if the object is
+          static. *)
 
-type typing = Inference | Identifier of string
+(** type information provied by users *)
+type typing =
+  | Inference
+      (** user omit the type information and let compiler to infer the type *)
+  | Identifier of string  (** user provide the name of the type*)
